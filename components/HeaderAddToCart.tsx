95418c32a794b8656b3ded178759cb3eef02fcd3
sandeepkrsaxena <sandeep.k9953@gmail.com>
2024-12-02 15:46:08 +0530

    add to cart page rename

--- components/HeaderAddToCart.tsx
+++ components/HeaderAddToCart.tsx
@@ -11,7 +11,7 @@ const HeaderAddToCart = () => {
       }
     }, []);
     return (
-        <Link href={`/add_to_cart`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Cart {cart.length ? cart.length : 0}</Link>
+        <Link href={`/addtocart`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Cart {cart.length ? cart.length : 0}</Link>
     )
 }
 export default HeaderAddToCart;
